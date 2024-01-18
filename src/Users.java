6e4623736cb9ec8fdf276218a5b1d22b88d336db
Giulia Regattieri <117976291+fs0ciet1@users.noreply.github.com>
2024-01-18 13:37:40 +0100

    uplod project

---
+++ src/Users.java
@@ -0,0 +1,15 @@
+public class Users
+{
+    //=================================== ATTRIBUTI ===================================//
+    private String name;
+    private String surname;
+    private Double email;
+
+    //=================================== COSTRUTTORE ===================================//
+    public Users(String username, String password , Double balance)
+    {
+        this.name =name;
+        this.surname =surname;
+        this.email =email;
+    }
+}
